83fc8af733b8da935bf666a98f7d726cf9090835
Admins <vothanhnho8005@gmail.com>
2023-05-07 19:48:48 +0700

    fixx bug

--- src/api/setupProxy.js
+++ src/api/setupProxy.js
@@ -1,11 +1,24 @@
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+
+// module.exports = function (app) {
+//   app.use(
+//     "/maps/api",
+//     createProxyMiddleware({
+//       target: "https://maps.googleapis.com",
+//       changeOrigin: true,
+//     })
+//   );
+// };
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/maps/api",
-    createProxyMiddleware({
+    createProxyMiddleware("/maps/api", {
       target: "https://maps.googleapis.com",
       changeOrigin: true,
+      pathRewrite: {
+        "^/maps/api": "", // Loại bỏ đường dẫn "/maps/api" khỏi yêu cầu
+      },
     })
   );
 };
