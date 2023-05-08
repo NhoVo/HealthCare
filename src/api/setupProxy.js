955962698a2014a9005e0db341b72c9a074894d4
Admins <vothanhnho8005@gmail.com>
2023-05-08 18:08:52 +0700

    11

--- src/api/setupProxy.js
+++ src/api/setupProxy.js
@@ -1,24 +1,11 @@
-// const { createProxyMiddleware } = require("http-proxy-middleware");
-
-// module.exports = function (app) {
-//   app.use(
-//     "/maps/api",
-//     createProxyMiddleware({
-//       target: "https://maps.googleapis.com",
-//       changeOrigin: true,
-//     })
-//   );
-// };
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function (app) {
-  app.use(
-    createProxyMiddleware("/maps/api", {
-      target: "https://maps.googleapis.com",
-      changeOrigin: true,
-      pathRewrite: {
-        "^/maps/api": "", // Loại bỏ đường dẫn "/maps/api" khỏi yêu cầu
-      },
-    })
-  );
+module.exports = function (req, res) {
+  return createProxyMiddleware({
+    target: "https://maps.googleapis.com",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/api/proxy": "/maps/api",
+    },
+  })(req, res);
 };
